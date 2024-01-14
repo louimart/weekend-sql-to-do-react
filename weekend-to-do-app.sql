1ccf3e98994c05386ec0802bcc9630d8921c7ccd
Louis Martin <louimart@gmail.com>
2024-01-13 20:01:03 -0600

    GET route functioning. Corrected error in SQL database naming

--- weekend-to-do-app.sql
+++ weekend-to-do-app.sql
@@ -3,3 +3,8 @@ CREATE TABLE "weekeend-to-do-app" (
 	"task" VARCHAR (250) NOT NULL,
 	"task-status" BOOLEAN DEFAULT FALSE,
 );
+
+INSERT INTO "weekend-to-do-app" ("task", "task-status")
+VALUES ('make bed', false),
+('wash dishes', false),
+('vacuum', false);
